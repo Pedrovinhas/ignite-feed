8355942bd8bffc8a07b8ca592772ce1a93db86dd
pedrovinhas <pstvideos@gmail.com>
2022-06-16 15:41:31 -0300

    ⚙ os Motores do React

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Sidebar } from "./components/Sidebar";
 import './global.css'
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/pedrovinhas.png',
+      name: 'Pedro Henrique Vinhas',
+      role: 'Desenvolvedor Júnior'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-06-13 20:42:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/dants0.png',
+      name: 'Guilherme Dantas Góes',
+      role: 'Desenvolvedor Júnior',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-06-13 20:52:00'),
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -15,10 +46,18 @@ export function App() {
         </aside>
      
       <main>
-      <Post 
-        author="Pedro Henrique Vinhas" 
-        content="Muito feliz em estar fazendo o curso Ignite!"
-      />
+      {
+        posts.map(post => {
+          return (
+           <Post
+             key={post.id}
+             author={post.author}
+             content={post.content}
+             publishedAt={post.publishedAt}
+           />
+          )
+        })
+      }
       </main>
       </div>
     </div>
